dc732caf9746cb9c596ccc070cdf8725b32a3912
Zaw Htet Aung <49681047+zawhtetaung2922@users.noreply.github.com>
2025-01-22 16:55:13 +0630

    Add files via upload

--- src/pages/My Account/MyAccount.jsx
+++ src/pages/My Account/MyAccount.jsx
@@ -1,107 +1,142 @@
 import React, { useState } from "react";
 import { FaUser } from "react-icons/fa";
+import axiosInstance from "../../api/axios";
+import { useAuth } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 import "./MyAccount.css";
 
 const MyAccount = () => {
+  const { login } = useAuth();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isRegistering, setIsRegistering] = useState(false);
-  const [accountInfo, setAccountInfo] = useState({ name: "", email: "", password: "" });
+  const [role, setRole] = useState("CUSTOMER"); // Default role
+  const [formData, setFormData] = useState({ name: "", email: "", password: "" });
   const [feedback, setFeedback] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
+  const navigate = useNavigate();
 
-  const handleAccountChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setAccountInfo({ ...accountInfo, [name]: value });
+    setFormData({ ...formData, [name]: value });
   };
 
-  const handleLogin = (e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-
-    if (accountInfo.email === "user@example.com" && accountInfo.password === "password" && accountInfo.name === "user") {
+    try {
+      const response = await axiosInstance.post(`/auth/login`, {
+        email: formData.email,
+        password: formData.password,
+      });
+      login(response.data, response.data.token);
       setIsLoggedIn(true);
       setFeedback("Login successful!");
-    } else {
-      setFeedback("Invalid email or password.");
+      setError(null);
+      if (response.data.role) {
+        navigate("/");
+      }
+    } catch (err) {
+      setError("Invalid login credentials");
+      setFeedback("");
     }
   };
 
-  const handleRegister = (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
-    
-    setFeedback("Registration successful! Please log in.");
-    setIsRegistering(false);
+    try {
+      await axiosInstance.post(`/auth/register`, { ...formData, role });
+      setSuccess(true);
+      setFeedback("Registration successful! Please log in.");
+      setIsRegistering(false);
+
+      // Navigate to the appropriate login page after registration
+      if (role.toUpperCase() === "CUSTOMER") {
+        navigate("/customer/login");
+      } else if (role.toUpperCase() === "AGENCY") {
+        navigate("/agency/login");
+      }
+    } catch (err) {
+      console.error("Registration failed:", err);
+      setError("Registration failed. Please try again.");
+    }
   };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
     setFeedback("You have been logged out.");
-    setAccountInfo({ email: "", password: "" });
+    setFormData({ name: "", email: "", password: "" });
   };
 
   return (
-    <>
-
     <div className="my-account">
-    <div className="loginBg"></div>
+      <div className="loginBg"></div>
       <div>
-        {!isLoggedIn ? (
+        
           <div className="auth-container">
-            <h2>{isRegistering ? "Register" : "Login"}</h2>
+            <h2>{isRegistering ? `${role} Register` : "Login"}</h2>
             <form onSubmit={isRegistering ? handleRegister : handleLogin}>
               {isRegistering && (
-                  <>
+              <>
+                <div className="role">
+                  <label htmlFor="role">Role:</label>
+                  <select
+                    id="role"
+                    name="role"
+                    value={role}
+                    onChange={(e) => setRole(e.target.value)}
+                    required
+                  >
+                    <option value="CUSTOMER">Customer</option>
+                    <option value="AGENCY">Agency</option>
+                  </select>
+                  </div>
                   <label htmlFor="name">Name:</label>
                   <input
-                      type="text"
-                      id="name"
-                      name="name"
-                      value={accountInfo.name || ""}
-                      onChange={handleAccountChange}
-                      required
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleInputChange}
+                    required
                   />
-                  </>
+                  
+                </>
               )}
               <label htmlFor="email">Email:</label>
               <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={accountInfo.email}
-                  onChange={handleAccountChange}
-                  required
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
               />
               <label htmlFor="password">Password:</label>
               <input
-                  type="password"
-                  id="password"
-                  name="password"
-                  value={accountInfo.password}
-                  onChange={handleAccountChange}
-                  required
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+              onChange={handleInputChange}
+              placeholder="Minimum of 10 characters long"
+                required
               />
               <button type="submit">{isRegistering ? "Register" : "Login"}</button>
-              </form>
-              <p>
-                  {isRegistering ? "Already have an account?" : "Don't have an account?"}{" "}
-
-                  <button className="toggle-auth" onClick={() => setIsRegistering(!isRegistering)}>
-                      {isRegistering ? "Login" : "Register"}
-                  </button>
-              </p>
-              {feedback && <p className="feedback-message">{feedback}</p>}
+            </form>
+            <p>
+              {isRegistering ? "Already have an account?" : "Don't have an account?"}{" "}
+              <button className="toggle-auth" onClick={() => setIsRegistering(!isRegistering)}>
+                {isRegistering ? "Login" : "Register"}
+              </button>
+            </p>
+            {feedback && <p className="feedback-message">{feedback}</p>}
+            {error && <p className="feedback-message error">{error}</p>}
+            {success && <p className="feedback-message">Registration successful!</p>}
           </div>
-          ) : (
-          <div className="logged-in-container">
-            <FaUser className="user-icon" />
-            <p>Welcome, {accountInfo.name}!</p>
-            <button onClick={handleLogout}>Logout</button>
-          </div>
-        )}
+        
+      </div>
     </div>
-    </div>
-    </>
-    
   );
 };
 
 export default MyAccount;
-
